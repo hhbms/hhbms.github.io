a43018dab68b72832b77812d58a41bb3ae6b718e
Matthew Feng <mattfeng@mit.edu>
2023-02-23 16:36:52 -0500

    update home

--- public-notes/src/pages/index.js
+++ public-notes/src/pages/index.js
@@ -2,27 +2,12 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import * as styles from "../styles/notes.module.scss"
+import IndexContent from "../content/index.mdx"
 
 const IndexPage = () => (
   <div className={styles.mainContainer}>
     <div>
-      <h1>MIT Huang-Hobbs BioMakerspace</h1>
-
-      <h2>Training modules</h2>
-      <ul>
-        <li>
-          <Link to="/microbiology">Microbiology and aseptic technique</Link>
-        </li>
-      </ul>
-
-      <h2>Protocols</h2>
-      <ul>
-        <li>
-          <Link to="/protocols#preparing-glycerol-stocks-of-bacteria">
-            Preparing glycerol stocks of bacteria
-          </Link>
-        </li>
-      </ul>
+      <IndexContent />
     </div>
   </div>
 )
